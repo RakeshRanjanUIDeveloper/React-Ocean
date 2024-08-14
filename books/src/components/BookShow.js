d94e1cb829af60b19480466afa70dbc52bc471bb
Rakesh Ranjan <rakesh.j.ranjan@accenture.com>
2024-08-14 16:05:27 +0530

    section 6 completed

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BookEdit from './BookEdit';
 
-const BookShow = () => {
+const BookShow = ({book, onDelete, onEdit}) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const handleEditClick = () =>{
+    setShowEdit(!showEdit)
+  }
+  const handleDeleteClick = () =>{
+    onDelete(book.id)
+  }
+  const handleSubmit = (id, newTitle) =>{
+    setShowEdit(false)
+    onEdit(id, newTitle)
+  }
+  let content = <h3>{book.title}</h3>;
+  if(showEdit){
+    content = <BookEdit book={book} onSubmit={handleSubmit} />
+  }
   return (
-    <div>BookShow</div>
+    <div className='book-show'>
+        <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt='book' />
+        <div>{content}</div>
+      <div className='actions'>
+        <button className='edit' onClick={handleEditClick}>
+          Edit
+        </button>
+        <button className='delete' onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
   )
 }
 
