8b5c69f6c07253ae8ec360f9a96e75d9a6a6ee8b
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2024-12-13 20:44:39 +0530

    react code

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -3,11 +3,22 @@ import UseState from './components/UseState'
 import UseState2 from './components/UseState2'
 import Theme from './components/Theme'
 import Theme2 from './components/Theme2'
+import Modal from './components/Modal'
+import Room from './components/Room'
+import CounterParent from './components/CounterParent'
+import DropdownParent from './components/DropdownParent'
+// import ModalParent from './components/ModalParent'
+import FetchUser from './components/FetchUser'
+import EvenLogger from './components/EvenLogger'
+import NestedDropdown from './components/NestedDropdown'
+import AddressParent from './components/AddressParent'
+import ResponsiveSlider from './components/ResponsiveSlider/ResponsiveSlider'
+import ModalParent from './components/Modal/ModalParent'
 
 const App = () => {
   return (
     <div>
-        <Theme2 />
+      <ModalParent />
     </div>
   )
 }
