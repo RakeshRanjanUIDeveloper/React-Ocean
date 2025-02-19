aabc79a4f2a636bbefd4967ac51ee23147898eaf
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-02-20 00:00:46 +0530

    sorting table

--- mcr-app/src/App.js
+++ mcr-app/src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../src/components/bookshelftapp/pages/Home";
 import SearchPage from "../src/components/bookshelftapp/pages/SearchPage";
 import './App.css'
+import SnackTable from "./components/snackTableApp/components/SnackTable";
 const App = () => {
   return (
-    <Router>
+    /* <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchPage />} />
       </Routes>
-    </Router>
+    </Router> */
+    <SnackTable />
   );
 };
 
