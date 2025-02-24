75e17639afde9408d05c8206ab5834ab1551f660
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-02-25 00:32:42 +0530

    set 3 completed

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import SetOne from './components/setone/SetOne'
+import SetThree from './components/setthree/SetThree'
 import SetTwo from './components/settwo/SetTwo'
 
 function App() {
   return (
     <div className='App'>
         {/* <SetOne /> */}
-        <SetTwo />
+        {/*   <SetTwo /> */}
+        <SetThree />
     </div>
   )
 }
