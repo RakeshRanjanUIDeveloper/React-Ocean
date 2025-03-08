012c1862d89b529b32aa2c3f4e3350c66122a1dc
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-03-08 15:31:31 +0530

    react code added

--- vite-project/src/components/setfive/SetFive.jsx
+++ vite-project/src/components/setfive/SetFive.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import OnlineUsers from './OnlineUsers'
+import Products from './Products'
+import Image from './Image'
+import Users from './Users'
+import Chat from './Chat'
+import Comments from './Comments'
 
 const SetFive = () => {
   return (
     <React.Fragment>
         <OnlineUsers />
+        <Products />
+        <Image heading='User Profile' width='200px' height='120px' />
+        <Users />
+        <Chat />
+        <Comments />
     </React.Fragment>
   )
 }
