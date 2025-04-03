3c771abb2f2b9dfa90c7c05a6a96175f06127a14
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-04-03 23:20:48 +0530

    AutoComplete SearchBar

--- akshay-machinecoding/src/App.js
+++ akshay-machinecoding/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import FileExplorer from './components/FileExplorer/FileExplorer';
 import ProgressMain from './components/ProgressBar/ProgressMain';
+import SearchBar from './components/SearchBar/SearchBar';
 function App() {
   return (
     <div className="App">
-{/* <ProgressMain /> */}
-    <FileExplorer />
+{/* <ProgressMain /> 
+    <FileExplorer />*/}
+    <SearchBar />
     </div>
   );
 }
