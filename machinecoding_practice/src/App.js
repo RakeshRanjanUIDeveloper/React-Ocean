8458470fa7573b55356d35aea52b22d1f061c8bc
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-05-08 00:12:43 +0530

    react code added

--- machinecoding_practice/src/App.js
+++ machinecoding_practice/src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
-import FileExplorer from './components/FileExplorer/FileExplorer';
-import ProgressMain from './components/ProgressBar/ProgressMain';
-import SearchBar from './components/SearchBar/SearchBar';
+import "./App.css";
+//import FileExplorer from "./components/FileExplorer/FileExplorer";
+import CustomButton from "./components/NeogCamp/PracticeSet1/CustomButton";
+import EmployeeCard from "./components/NeogCamp/PracticeSet1/EmployeeCard";
+import StationeryList from "./components/NeogCamp/PracticeSet1/StationeryList";
+//import ProgressMain from "./components/ProgressBar/ProgressMain";
+//import SearchBar from "./components/SearchBar/SearchBar";
 function App() {
+  const btnStyle = {
+    backgroundColor: "lightgreen",
+    color: "darkgreen",
+    borderRadius: "5px",
+    padding: "10px",
+  };
+  const header ="Stationery Items";
+  const items = ['pen', 'pencil', 'ruler', 'eraser']
   return (
     <div className="App">
-{/* <ProgressMain /> 
-    <FileExplorer />*/}
-    <SearchBar />
+      {/* <ProgressMain /> 
+    <FileExplorer />
+    <SearchBar />*/}
+
+      <EmployeeCard
+        name="Rakesh"
+        designation="Software Engineer"
+        workExperience="10 Years"
+      />
+      <CustomButton text="Start" btnStyle={btnStyle} />
+      <StationeryList header={header} items={items} />
     </div>
   );
 }
