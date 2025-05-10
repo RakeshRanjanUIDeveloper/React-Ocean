d2aafe7ba47a2ea62b32eb4c598a83edd8a64763
RakeshRanjanUIDeveloper <rakeshmodi034@gmail.com>
2025-05-10 16:46:18 +0530

    practice set 1 completed

--- machinecoding_practice/src/App.js
+++ machinecoding_practice/src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 //import FileExplorer from "./components/FileExplorer/FileExplorer";
 import CustomButton from "./components/NeogCamp/PracticeSet1/CustomButton";
+import CustomImage from "./components/NeogCamp/PracticeSet1/CustomImage";
+import Employee from "./components/NeogCamp/PracticeSet1/Employee";
 import EmployeeCard from "./components/NeogCamp/PracticeSet1/EmployeeCard";
+import Products from "./components/NeogCamp/PracticeSet1/Products";
 import StationeryList from "./components/NeogCamp/PracticeSet1/StationeryList";
+import Student from "./components/NeogCamp/PracticeSet1/Student";
 //import ProgressMain from "./components/ProgressBar/ProgressMain";
 //import SearchBar from "./components/SearchBar/SearchBar";
 function App() {
@@ -14,6 +18,23 @@ function App() {
   };
   const header ="Stationery Items";
   const items = ['pen', 'pencil', 'ruler', 'eraser']
+  const imageLink =
+  'https://cdn.pixabay.com/photo/2023/03/18/10/43/plum-blossoms-7860381_1280.jpg'
+ const caption = 'Spring Flowers'
+ const products = [
+  { name: 'Perk', quantity: 10, sales: 7 },
+  { name: 'Pepsi', quantity: 10, sales: 20 },
+  { name: 'Coke', quantity: 18, sales: 50 },
+  { name: 'Maggi', quantity: 41, sales: 22 },
+  { name: '5Star', quantity: 7, sales: 9 },
+ ]
+ const student = {
+  name: 'John Doe',
+  english: 90,
+ maths: 80,
+ computers: 70,
+}
+
   return (
     <div className="App">
       {/* <ProgressMain /> 
@@ -27,6 +48,10 @@ function App() {
       />
       <CustomButton text="Start" btnStyle={btnStyle} />
       <StationeryList header={header} items={items} />
+      <CustomImage imageLink={imageLink} caption={caption} />
+      <Products products={products} />
+      <Student student={student} />
+      <Employee  />
     </div>
   );
 }
